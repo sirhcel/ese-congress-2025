548e23aab808878d1868e7c0c096cb72da2be04e
Christian Meusel <christian.meusel@posteo.de>
2025-12-03 22:19:02 +0100

    Use final regular slide as number of slides in footer

--- slides/global-bottom.vue
+++ slides/global-bottom.vue
@@ -1,5 +1,5 @@
 <template>
   <footer v-if="$slidev.nav.currentLayout !== 'cover'" class="absolute bottom-0 right-0 p-2 text-gray-400">
-    <SlideCurrentNo/>/<SlidesTotal/>
+    <SlideCurrentNo/>/46<!--<SlidesTotal/>-->
   </footer>
 </template>
